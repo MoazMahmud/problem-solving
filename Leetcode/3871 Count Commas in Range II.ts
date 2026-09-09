bd189d2992dd5bc155e9a5b4edf42e995785bf8d
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2026-09-09 12:41:19 +0600

    Update 3871 Count Commas in Range II.ts

--- Leetcode/3871 Count Commas in Range II.ts
+++ Leetcode/3871 Count Commas in Range II.ts
@@ -1,9 +1,9 @@
 function countCommas(n: number): number {
-    let lo = 1, hi = 1000, stdCommas = 0, totalCommas = 0;;
-    for (; hi <= n * 1000; lo = hi, hi *= 1000, stdCommas++) {
-        const rangeHigh = Math.min(n, hi - 1);
-        const commaCount = (rangeHigh - lo + 1) * stdCommas;
-        totalCommas += commaCount;
+    let total = 0;
+    let lo = 1;
+    for (let commasPerNumber = 0; lo <= n; lo *= 1000, commasPerNumber++) {
+        const hi = Math.min(n, lo * 1000 - 1);
+        total += (hi - lo + 1) * commasPerNumber;
     }
-    return totalCommas;
+    return total;
 }
